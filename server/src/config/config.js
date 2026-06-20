e802f9e8e09c0ee10a1c46ce180773bfec06a2d5
daniji09 <daniji09@ucm.es>
2026-06-20 11:09:45 +0200

    Deployment config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -5,3 +5,4 @@ export const DB_HOST = process.env.DB_HOST;
 export const DB_PORT = process.env.DB_PORT;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_TEST_NAME = process.env.DB_TEST_NAME;
+export const FIREBASE_JSON = process.env.FIREBASE_JSON;
